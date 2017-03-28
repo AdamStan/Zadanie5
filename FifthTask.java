bad111ec3004821e0e13a69b8744e642e156c10f
Adam <adam15stan@gmail.com>
2017-03-28 19:40:00 +0200

    Z zajęć co zrobiłem

--- FifthTask.java
+++ FifthTask.java
@@ -1,8 +1,8 @@
 package zadanie5;
 /**
- *Napisz program proszacy o podanie 2 wektorow. Koniec wektora oznacza sie za pomoca wcisniecia klawisza 
+ * Napisz program proszacy o podanie 2 wektorow. Koniec wektora oznacza sie za pomoca wcisniecia klawisza 
  * enter. Jezeli podany ciag nie jest liczba, jest ignorowany. Nastepnie nalezy sprobowac dodac wektory, 
- * jezeli sa rownej dlugosci. Jezeli nie, sa, rzucany jest wlasny wyjatek WektoryRoznejDlugosciException, 
+ * jezeli sa rownej dlugosci. Jezeli nie sa, rzucany jest wlasny wyjatek WektoryRoznejDlugosciException, 
  * za pomoca ktorego mozna podac a nastepnie odczytac dlugosci tych wektorow. Jezeli sa rownej dlugosci, 
  * wynik dodawania zapisywany jest do pliku. Jezeli nie sa rownej dlugosci, uzytkownik jest proszony o 
  * ponowne wprowadzenie tych wektorow.
@@ -14,36 +14,42 @@ package zadanie5;
  import java.util.Scanner;
 
 public class FifthTask {
+	public static void main(String[] args) throws Exception {
+// 		try{
+			Integer[] w1;
+			Integer[] w2;
+			Scanner odczyt1,odczyt2;
+			
+			w1 = new Integer[1];
+			
+			do{
+			  System.out.println("Podaj pierwszy wektor ");
+			  odczyt1 = new Scanner(System.in);
+			}while(odczyt1.hasNextInt() == false);
+			
+			Wektor.odczyt(odczyt1);
+			
+			do{
+			  System.out.println("Podaj drugi wektor ");
+			  odczyt2  = new Scanner(System.in);
+			}while(odczyt2.hasNextInt() == false);
+			
+			Wektor.odczyt(odczyt2);
+			
+			if(){
+				double result = vectorFirst.dlugosc()+vectorSecond.dlugosc();
+				zapis_nio(result,"wynik_dodawania.txt");
+				System.out.println("Wektory są tej samej długości");
+			}
+			else throw new WektoryRoznejDlugosciException();
+		} catch (WektoryRoznejDlugosciException e) {
+			//e.printStackTrace();
+		}*/
+	}
 	public static void zapis_nio(double wynik, String name) throws IOException{ // zrobić tak by nie nadpisywał
 		FileChannel fc = new FileOutputStream(name).getChannel();
 		String buff;
 		buff = String.valueOf(wynik);
 		fc.write(ByteBuffer.wrap(buff.getBytes()));
-	}
-	public static void main(String[] args) throws Exception {
-		try{
-			int w1,w2;
-			System.out.println("Podaj pierwszy wektor ");
-			Scanner odczyt1  = new Scanner(System.in);
-			w1 = odczyt1.nextInt();
-			w2 = odczyt1.nextInt();
-			Wektor vectorFirst = new Wektor(w1,w2);
-			System.out.println("Podaj drugi wektor ");
-			Scanner odczyt2  = new Scanner(System.in);
-			w1 = odczyt1.nextInt();
-			w2 = odczyt1.nextInt();
-			Wektor vectorSecond = new Wektor(w1,w2);
-			System.out.println("Długość wektora jest równa "+ vectorFirst.dlugosc());
-			System.out.println("Długość wektora jest równa "+ vectorSecond.dlugosc());
-			if(vectorFirst.dlugosc() == vectorSecond.dlugosc()){
-				double result = vectorFirst.dlugosc()+vectorSecond.dlugosc();
-				zapis_nio(result,"wyniki.txt");
-			}
-			else throw new WektoryRoznejDlugosciException();
-		} catch (WektoryRoznejDlugosciException e) {
-			//e.printStackTrace();
-		} catch (java.util.InputMismatchException e) {
-			System.out.println("Wspolrzedne wektora muszą być oddzielone spacjami!");
-		}
 	}
 }
--- Wektor.java
+++ Wektor.java
@@ -1,24 +1,25 @@
 package zadanie5;
-/*przeciązyc operator== :-)
-Wyjdzie super rzecz*/
+/*Tu bedzie się działo, dużo różnych meteod do obsługi jebanych wektorów*/
+ import java.util.Scanner;
 public class Wektor {
-    private int x;
-    private int y;
-    public Wektor(){
-        x = 0;
-        y = 0;
-    }
-    public Wektor(int a, int b){
-        x = a;
-        y = b;
-    }
-    public Wektor(Wektor w){
-        x = w.x;
-        y = w.y;
-    }
-    public double dlugosc(){
-        double d;
-        d = Math.sqrt(x*x + y*y);
-        return d;
-    }
+	/*public Integer[] dodaj(Integer[] a, Integer[] b){
+	  
+	  Integer[] buff;
+	  Integer sizeVector = a.size();
+	  buff = new Integer[sizeVector];
+	  for(int i = 0; i<sizeVector;i++)
+	    buff[i] = a[i] + b[i];
+	   
+	  return buff;
+	}*/
+	public static /*Integer[]*/void odczyt(Scanner scan){
+	  String wektor;
+	  wektor = scan.nextLine();
+	  System.out.println(wektor+" "+ wektor.length());
+	  while(){
+	  //bawimy sie, w stringu poszukamy liczb:
+	  //wpisz w google "jak poszukać liczb w stringu?"
+	  }
+	  Integer[] wektor_int;
+	}
 }
